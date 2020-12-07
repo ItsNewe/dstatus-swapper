89f66cbbeff13b6e7fa8fd04361fd8793375c0e9
Newe <speedy.wolfy@outlook.com>
2020-12-07 14:12:18 +0100

    Added emoji support, config switch from json -> py dict

--- main.py
+++ main.py
@@ -2,29 +2,23 @@ import json
 from time import sleep
 import discord
 from discord.ext import tasks
+import requests
+from hyper.contrib import HTTP20Adapter
+from config import configDict
 
-t=None
 client = discord.Client()
-statusList=[]
 
-with open("./config.json", "r") as f:
-	jsonRaw= json.load(f)
-	t=jsonraw["token"]
-
-	for i in jsonRaw["statuses"]:
-		statusList.append((i[0], i[1]))
-
-@tasks.loop(seconds=1)
 async def status():
-	print("Status loop: Index 0")
-	for i in statusList:
-		print(f"Defining status: {i[0]")
-		await client.change_presence(status=discord.Status.idle, activity=discord.Activity(name="Custom status", type=4, state=i[0]))
-		sleep(int(i[1]))
+	for i in configDict["statuses"]:
+		print(f"Defining status: {i[0]} : {i[1]}")
+		
+		await client.change_presence(status=discord.Status.idle, activity=discord.Activity(name="Custom Status", type=4, state=i[1], emoji={"name":i[0], "animated":False}))
+		sleep(i[2])
 
 @client.event
 async def on_ready():
 	print('Ready')
-	status.start()
+	while 1:
+		await status()
 	
-client.run(t, bot=False)
+client.run(configDict["token"], bot=False)
